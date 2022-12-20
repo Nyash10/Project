f40338b8ea9a0513a81505ea53f1fd9293691fbd
ChoonXiangg <119977822+ChoonXiangg@users.noreply.github.com>
2022-12-20 04:00:04 +0000

    Add files via upload

---
+++ keyboard.js
@@ -0,0 +1,30 @@
+function buyQ65(){
+    var quantity=prompt("Quantity");
+    quant=parseInt(quantity);
+    var total=quant*239;
+    window.alert("The total is €"+total);
+}
+function buyK8(){
+    var quantity=prompt("Quantity");
+    quant=parseInt(quantity);
+    var total=quant*119;
+    window.alert("The total is €"+total);
+}
+function buyK5(){
+    var quantity=prompt("Quantity");
+    quant=parseInt(quantity);
+    var total=quant*99;
+    window.alert("The total is €"+total);
+}
+function buyK3(){
+    var quantity=prompt("Quantity");
+    quant=parseInt(quantity);
+    var total=quant*94;
+    window.alert("The total is €"+total);
+}
+function buyQ10(){
+    var quantity=prompt("Quantity");
+    quant=parseInt(quantity);
+    var total=quant*215;
+    window.alert("The total is €"+total);
+}
